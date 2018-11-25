3c9ee17a22199ff72c74f6700c1a7f59fb0fd1b1
AhmetKursatEsim <kursatesim@gmail.com>
2018-11-25 20:28:38 +0300

    ga with parsing areaa

---
+++ GA.py
@@ -0,0 +1,9 @@
+
+class Population:
+   def __init__(self):
+      self.Popsize=10
+      self.Individuals = [10]
+      self.fittest=0
+   def InıtalizePopulation(self):
+      for  i in  range (len(self.Individuals)):
+         self.Individuals[i]=new self.Individuals
